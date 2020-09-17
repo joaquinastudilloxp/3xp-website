c07e5ea6be685fb74df039b4b02f6f008b383a97
Joaquin Astudillo <joaquin.astudillo@xtremepush.com>
2020-09-17 12:51:54 +0100

    Add basic login system to repository

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes/index";
+import * as firebase from "firebase";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+const configOptions = {
+	apiKey: "AIzaSyB0baJ7Rc7V8FWtJE6Qkb_Zua_MayfSpxY",
+	authDomain: "xp-firebase-auth.firebaseapp.com",
+	databaseURL: "https://xp-firebase-auth.firebaseio.com",
+	projectId: "xp-firebase-auth",
+	storageBucket: "xp-firebase-auth.appspot.com",
+	messagingSenderId: "801792621127",
+	appId: "1:801792621127:web:8724063123590584a88f62",
+	measurementId: "G-2NEKZYLG37"
+};
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/routes/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Dashboard from '../components/Dashboard'
+import firebase from "firebase";
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+		{
+			path: '/login',
+			name: 'login',
+			component: Login,
+			meta: {
+				hideForAuth: true
+			}
+		},
+		{
+			path: '/register',
+			name: 'Register',
+			component: Register,
+			meta: {
+				hideForAuth: true
+			}
+		},
+		{
+			path: '/dashboard',
+			name: 'Dashboard',
+			component: Dashboard,
+			meta: {
+				requiresAuth: true
+			}
+		}
+	]
+});
+
+router.beforeEach((to, from, next) => {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (to.matched.some(record => record.meta.requiresAuth)) {
+			if (!user) {
+				next({ path: '/login' });
+			} else {
+				next();
+			}
+
+		} else {
+			next();
+		}
+
+		if (to.matched.some(record => record.meta.hideForAuth)) {
+			if (user) {
+				next({ path: '/dashboard' });
+			} else {
+				next();
+			}
+		} else {
+			next();
+		}
+	});
+});
+
+export default router
